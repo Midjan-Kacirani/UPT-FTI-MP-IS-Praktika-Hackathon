3eedab742e6600498c12a5cfcf92e5d8ef04255c
Midjan <midjankacirani@gmail.com>
2024-06-28 11:38:47 +0200

    First commit: Application setup, database setup and configuration, and model creation.

---
+++ DevManagementSystem/src/main/java/com/hackathonpraktika/DevManagementSystem/model/DevSkillExp.java
@@ -0,0 +1,34 @@
+package com.hackathonpraktika.DevManagementSystem.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "DevSkillExp")
+@IdClass(DevSkillExpId.class)
+@Data
+public class DevSkillExp {
+
+    @Id
+    @Column(name = "PersonId")
+    private Long personId;
+
+    @Id
+    @Column(name = "DevSkillId")
+    private Long devSkillId;
+
+    @Column(name = "YearOfExp", nullable = false)
+    private int yearOfExp;
+
+    @ManyToOne
+    @JoinColumn(name = "PersonId", insertable = false, updatable = false)
+    private Person person;
+
+    @ManyToOne
+    @JoinColumn(name = "DevSkillId", insertable = false, updatable = false)
+    private DevSkill devSkill;
+
+    // Getters and setters
+    // ...
+
+}
---
+++ DevManagementSystem/src/main/java/com/hackathonpraktika/DevManagementSystem/model/Person.java
@@ -0,0 +1,37 @@
+package com.hackathonpraktika.DevManagementSystem.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Person")
+@Data
+public class Person {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PersonId")
+    private Long personId;
+
+
+    @Column(name = "Name", nullable = false)
+    private String name;
+
+    @Column(name = "Surname", nullable = false)
+    private String surname;
+
+    @Column(name = "Role", nullable = false)
+    private String role;
+
+    @Column(name = "Email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "Password", nullable = false)
+    private String password;
+
+    @Column(name = "profilePicture")
+    private String profilePicture;
+
+}
+
